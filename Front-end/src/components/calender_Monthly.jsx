d4a8b4b94228255825e94079d7e6b42c6561388a
Lange <cph-na157@cphbusiness.dk>
2020-11-26 15:29:11 +0100

    Kalender indsat ved usersite ved kalender

---
+++ Front-end/src/components/calender_Monthly.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  MonthView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+//import appointments  from '../demo-data/today-appointments';
+
+export default function Month() {
+  const appointments = [{title: 'Install New Router in Dev Room',
+    startDate: '2020-11-23T11:00', endDate: '2020-11-25T12:00',
+  }]
+   const [ data, setdata ] = useState({
+       data: appointments
+   ,
+    currentDate: new Date(),
+  });
+    
+      
+ 
+      
+    return (
+      <Paper>
+        <Scheduler data={data.data}>
+          <ViewState currentDate={data.currentDate}/>
+          <MonthView />
+          <Appointments />
+        </Scheduler>
+      </Paper>
+    );
+  }
