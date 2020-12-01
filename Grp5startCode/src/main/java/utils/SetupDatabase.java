1376d17ddbad237a586f8c7a10305ec9745a6f30
Lange <cph-na157@cphbusiness.dk>
2020-12-01 09:46:28 +0100

    rettet fejl ved setup, pga navngivning

--- Grp5startCode/src/main/java/utils/SetupDatabase.java
+++ Grp5startCode/src/main/java/utils/SetupDatabase.java
@@ -5,6 +5,7 @@
  */
 package utils;
 
+import entities.Category;
 import entities.Role;
 //pending implementation...
 //import entities.Category;
@@ -20,49 +21,49 @@ public class SetupDatabase {
     public static void main(String[] args) {
 
         //pending implementation...
-        //setupCategories();
+        setupEventCategories();
         
         //not planned yet 
-        //setupRoles();
+//        setupRoles();
     }
 
-    private static void setupRoles() {
-
-        EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-
-        Role r1 = new Role("Role-1");
-        Role r2 = new Role("Role-2");
-        Role r3 = new Role("Role-3");
-
-        em.persist(r1);
-        em.persist(r2);
-        em.persist(r3);
-
-        em.getTransaction().commit();
-    }
-
-//    private static void setupEventCategories() {
+//    private static void setupRoles() {
 //
 //        EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
 //        EntityManager em = emf.createEntityManager();
 //
 //        em.getTransaction().begin();
 //
-//        Category c1 = new Category("Vacation");
-//        Category c2 = new Category("Birthday");
-//        Category c3 = new Category("Reminder");
-//        Category c4 = new Category("Work appointment");
+//        Role r1 = new Role("Role-1");
+//        Role r2 = new Role("Role-2");
+//        Role r3 = new Role("Role-3");
 //
-//        em.persist(c1);
-//        em.persist(c2);
-//        em.persist(c3);
-//        em.persist(c4);
+//        em.persist(r1);
+//        em.persist(r2);
+//        em.persist(r3);
 //
 //        em.getTransaction().commit();
-//
 //    }
+
+    private static void setupEventCategories() {
+
+        EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Category c1 = new Category("Vacation");
+        Category c2 = new Category("Birthday");
+        Category c3 = new Category("Reminder");
+        Category c4 = new Category("Work appointment");
+
+        em.persist(c1);
+        em.persist(c2);
+        em.persist(c3);
+        em.persist(c4);
+
+        em.getTransaction().commit();
+
+    }
 
 }
