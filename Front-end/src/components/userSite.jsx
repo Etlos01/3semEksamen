8d640cf4e3ad1206f6c4c1db0d7e55b5e4274a18
Lange <cph-na157@cphbusiness.dk>
2020-12-03 10:18:04 +0100

    Små rettelser

--- Front-end/src/components/userSite.jsx
+++ Front-end/src/components/userSite.jsx
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
-import facade from "./apiFacade";
-import { UserUrlUserCount } from "./../sites";
+import React from "react";
 import Calendermonthly from './calender_Monthly';
 import Picker from './calender_components/datePicker'
-
-const url = UserUrlUserCount;
-
+/*<p>
+Her skal der vises en kalender.
+<br/> husk:
+<br/>npm install @material-ui/core og 
+npm i --save @devexpress/dx-react-core @devexpress/dx-react-scheduler samt
+npm i --save @devexpress/dx-react-scheduler-material-ui <br/>
+og npm i @date-io/date-fns@1.x date-fns
+</p>*/
 const UserSite = () => {
-  const [count, setCount] = useState("");
+  
   return (
     <>
-    <br/>
+    
     <br/>
         <Picker/>
-      <p>
-        Her skal der vises en kalender.
-        <br/> husk:
-        <br/>npm install @material-ui/core og 
-        npm i --save @devexpress/dx-react-core @devexpress/dx-react-scheduler samt
-        npm i --save @devexpress/dx-react-scheduler-material-ui <br/>
-        og npm i @date-io/date-fns@1.x date-fns
-      </p>
+        <br/>
+        <br/>
       <Calendermonthly/>
     </>
   );
