18b90d393436ef0e46f90c6f076ee4a87db97270
Lange <cph-na157@cphbusiness.dk>
2020-12-03 15:06:46 +0100

    Fået holidays ind og vises på kalenderen

--- Front-end/src/components/userSite.jsx
+++ Front-end/src/components/userSite.jsx
@@ -1,25 +1,17 @@
 import React, { useState } from "react";
 import facade from "./apiFacade";
-import { UserUrlUserCount } from "./../sites";
+
 import Calender_Monthly from './calender_Monthly';
 
 
-const url = UserUrlUserCount;
+
 
 const UserSite = () => {
-  const [count, setCount] = useState("");
+
+
   return (
     <>
-      <p>Number of users on this site: {count}</p>
-      <button
-        onClick={() =>
-          fetch(url, facade.makeOptions("GET", true))
-            .then((res) => res.json())
-            .then((data) => setCount(data))
-        }
-      >
-        Hente antal bruger
-      </button>
+     
 
       <p>
         Her skal der vises en kalender.
